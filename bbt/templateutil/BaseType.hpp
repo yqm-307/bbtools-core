4c31c17e91d2dca326ffc63aed8644916057da49
yqm-307 <979336542@qq.com>
2023-07-21 03:24:48 +0000

    BaseType中添加unique_ptr和weak_ptr

--- bbt/templateutil/BaseType.hpp
+++ bbt/templateutil/BaseType.hpp
@@ -8,14 +8,25 @@ template<class T>
 class BaseType
 {
 public:
-    typedef std::shared_ptr<T> SPtr;
-    typedef T*  RawPtr;
+    /* 共享指针 */
+    typedef std::shared_ptr<T>  SPtr;
+    typedef const SPtr          C_SPtr;
 
-    typedef const SPtr ConstSPtr;
-    typedef const T*  ConstRawPtr;
+    /* 单一指针 */
+    typedef std::unique_ptr<T>  UQPtr;
+    typedef const UQPtr         C_UQPtr;
 
-    typedef T&  Ref;
-    typedef const T& ConstRef;
+    /* 弱引用指针 */
+    typedef std::weak_ptr<T>    WkPtr;
+    typedef const WkPtr         C_WkPtr;
+
+    /* 原始指针 */
+    typedef T*                  RawPtr;
+    typedef const T*            C_RawPtr;
+
+    /* 引用 */
+    typedef T&                  Ref;
+    typedef const T&            C_Ref;
 };
 
 }
