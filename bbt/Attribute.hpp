7f93ce04cef4f7a5bf21037c2728733f9e149185
yqm-307 <979336542@qq.com>
2023-09-05 13:45:09 +0000

    gcc 函数属性添加。并加入判断宏

--- bbt/Attribute.hpp
+++ bbt/Attribute.hpp
@@ -22,25 +22,56 @@
  * @brief 提示函数的返回值需要被处理
  * 
  */
-#define BBTATTR_FUNC_RetVal [[nodiscard("返回值(错误码)需要进行处理")]]
-
+#if __glibc_has_attribute(warn_unused_result) >= 0
+#define BBTATTR_FUNC_RetVal __attribute__((warn_unused_result))
+#else
+#error "not exist attribute [warn_unused_result]"
+#endif
 
 /**
  * @brief 冷调用，对于少见的错误处理函数，可以使用来提高cpu分支预测的性能
+ * 根据局部性原理，冷函数的定义放在一起可以更有效的提高性能
  * 
+ * 尚未验证，也许是g++不支持
  */
+#if __glibc_has_attribute(cold) >= 0
 #define BBTATTR_FUNC_Cold __attribute__((cold))
+#else
+#error "not exist attribute [cold]"
+#endif
 
+/**
+ * @brief 热调用，对于热点函数，可以使用来提示编译器进行更积极的优化
+ * 根据局部性原理，热函数的定义放在一起可以更有效的提高性能
+ * 
+ * 尚未验证，也许是g++不支持
+ */
+#if __glibc_has_attribute(hot) >= 0
+#define BBTATTR_FUNC_Hot __attribute__((hot)) 
+#else
+#error "not exist attribute [hot]"
+#endif
 
 /**
  * @brief 将此函数被标记为启用的，并在调用时做编译期检测。携带弃用说明
  * 如果被标记函数被调用，则报warning
  */
+#if __glibc_has_attribute(__deprecated__) >= 0
 #define BBTATTR_FUNC_DeprecatedMsg(msg)    __attribute_deprecated_msg__(msg)
-
+#else
+#error "not exist attribute [__deprecated__]"
+#endif
 
 /**
  * @brief 将此函数被标记为启用的，并在调用时做编译期检测。
  * 如果被标记函数被调用，则报warning
  */
+#if __glibc_has_attribute(__deprecated__) >= 0
 #define BBTATTR_FUNC_Deprecated         __attribute_deprecated__
+#else
+#error "not exist attribute [__deprecated__]"
+#endif
+
+/**
+ * //TODO gcc还有一些很有用的attribute，比如内存检测、套接字检测等。
+ */
--- example/attribute/attributetest.cc
+++ example/attribute/attributetest.cc
@@ -1,18 +1,59 @@
 #include "bbt/Attribute.hpp"
-
+#include <stdio.h>
+#include "bbt/timer/Interval.hpp"
 void ColdFunc() BBTATTR_FUNC_Cold;
 
 // void foo1() BBTATTR_FUNC_DeprecatedMsg("foo1不好");
 // void foo1() BBTATTR_FUNC_Deprecated;
 // void foo1() BBTATTR_FUNC_Unavailable;
-void foo1() BBTATTR_FUNC_UnavailableMsg("foo1 不可以用");
+
+// int hot1(int i) BBTATTR_FUNC_Hot;
+// int hot2(int i) BBTATTR_FUNC_Hot;
+
+int hot1(int i);
+int hot2(int i);
+
+void test1()
+{
+    int a = 0;
+    bbt::timer::interval clock;
+    for(int i=0; i<__INT32_MAX__; ++i)
+    {
+        a = hot1(a);
+        a = hot2(a);
+    }
+    printf("result: %d\n", a);
+    printf("耗时: %ld\n", clock.intervalnow());
+}
+
+__attribute__((noclone)) void* noclone(int);
+void noclone1(void*(int));
+
+int nodiscord() BBTATTR_FUNC_RetVal;
 
 int main()
 {
-    ColdFunc();
-    foo1();
+    // ColdFunc();
+    // foo1();
+    test1();  
+    void*(*fptr)(int) = noclone;
+    noclone1(noclone); 
+    nodiscord();
 }
 
+int nodiscord()
+{
+    return 1;
+}
+
+void* noclone(int) 
+{return nullptr;}
+
+
+void noclone1(void*(int))
+{
+
+}
 
 void ColdFunc()
 {
@@ -20,4 +61,14 @@ void ColdFunc()
 
 void foo1()
 {
+}
+
+int hot1(int i)
+{
+    return ++i;
+}
+
+int hot2(int i)
+{
+    return --i;
 }
