4d0a45feb4bd60a0a0081efa046bbdd080c20ab7
yqm-307 <979336542@qq.com>
2023-09-22 20:51:39 +0800

    多线程测试通过

--- unit_test/Test_generate_id.cc
+++ unit_test/Test_generate_id.cc
@@ -18,8 +18,47 @@ BOOST_AUTO_TEST_CASE(t_increment_id)
         int64_t id = G.GenerateID();
         auto it = set1.find(id);
         BOOST_CHECK(it == set1.end());
+        set1.insert(id);
     }
 
+    bbt::uuid::EasyID<bbt::uuid::EM_AUTO_INCREMENT, 1> G2;
+
+    for( int i = 0; i<1000000; ++i )
+    {
+        int64_t id = G2.GenerateID();
+        auto it = set1.find(id);
+        BOOST_CHECK(it != set1.end());
+    }
+}
+
+BOOST_AUTO_TEST_CASE(t_thread_safe_id)
+{
+    bbt::uuid::EasyID<bbt::uuid::EM_AUTO_INCREMENT_SAFE, 0> G1;
+    std::set<int64_t> set1;
+    std::mutex  mtx;
+
+    std::vector<std::thread*> vec1;
+    for(int i = 0; i<50; ++i)
+    {
+        auto t = new std::thread([&](){
+            for(int i = 0; i<100000; ++i)
+            {
+                auto id = G1.GenerateID();
+                {
+                    std::lock_guard<std::mutex> lock(mtx);
+                    BOOST_CHECK(set1.find(id) == set1.end());
+                    set1.insert(id);
+                }
+            }
+        });
+        vec1.push_back(t);
+    }
+
+    for(auto && t : vec1)
+    {
+        t->join();
+    }
+    
 }
 
 
