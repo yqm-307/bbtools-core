e7a311fb91a9753a897c7d01aa2cd839d7692c74
yqm-307 <979336542@qq.com>
2024-05-11 10:29:22 +0800

    IPAddress添加std::hash模板特例化

--- bbt/base/myhash/UuidMyHash.hpp
+++ bbt/base/myhash/UuidMyHash.hpp
@@ -1,10 +1,12 @@
 #pragma once
+#include "bbt/base/net/IPAddress.hpp"
 #include "bbt/base/uuid/Uuid.hpp"
 #include "bbt/base/myhash/BKDR.hpp"
 
 namespace std
 {
-    template<> struct hash<bbt::uuid::UuidBase>
+    template<> 
+    struct hash<bbt::uuid::UuidBase>
     {
         typedef bbt::uuid::UuidBase argument_type;
         typedef std::size_t result_type;
@@ -16,4 +18,17 @@ namespace std
         }
     };
     
+    template<>
+    struct hash<bbt::net::IPAddress>
+    {
+        typedef bbt::net::IPAddress argument_type;
+        typedef std::size_t result_type;
+
+        result_type operator()(const argument_type& arg) const
+        {
+            const char* data = arg.GetIPPort().c_str();
+            size_t len = arg.GetIPPort().size();
+            return bbt::hash::BKDR::BKDRHash(data, len);
+        }
+    };
 }
