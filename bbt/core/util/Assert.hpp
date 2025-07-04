ba843902d290f6c504e4548e1260b3f8d3b827a4
= <=>
2025-07-05 02:37:33 +0800

    优化 Assert 宏定义，增强调试信息输出；修正条件编译指令

--- bbt/core/util/Assert.hpp
+++ bbt/core/util/Assert.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <cassert>
+#include <iostream>
 #include <bbt/core/Define.hpp>
 
 
@@ -8,22 +9,40 @@
 /////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////
 
-#ifdef BBT_DEBUG
+#if defined(NDEBUG)
 #define DebugAssertWithInfo(expr, info) \
     assert( (expr) && (info) )
 #else
 #define DebugAssertWithInfo(expr, info) {}
 #endif
 
-#ifdef BBT_DEBUG
+#if defined(NDEBUG)
 #define DebugAssert(expr) \
     assert( expr )
 #else
 #define DebugAssert(expr) {}
 #endif
 
+#if defined(NDEBUG)
+#define Assert(expr) \
+    do { \
+        if (bbt_unlikely(!(expr))) { \
+            std::cerr << "Assertion failed: " << #expr << " at " << __FILE__ << ":" << __LINE__ << std::endl; \
+        } \
+    } while(0)
+#else
 #define Assert(expr) \
     assert( expr )
+#endif
 
+#if defined(NDEBUG)
+#define AssertWithInfo(expr, info) \
+    do { \
+        if (bbt_unlikely(!(expr))) { \
+            std::cerr << "Assertion failed: " << #expr << " (" << info << ") at " << __FILE__ << ":" << __LINE__ << std::endl; \
+        } \
+    } while(0)
+#else
 #define AssertWithInfo(expr, info) \
     assert( (expr) && (info) )
+#endif
